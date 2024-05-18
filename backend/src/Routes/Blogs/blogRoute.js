6221fce421451aa0c2d3dc33866e1c60e3fcd1d0
arashagp <arash.qardashpoor@gmail.com>
2024-05-18 20:24:53 +0330

    inititall

--- backend/src/Routes/Blogs/blogRoute.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getBlogs } from "../../Controllers/Blogs/getBlogController.js";
-import { createBlog } from "../../Controllers/Blogs/createBlog.js";
-
-const blogRoute = express.Router();
-
-blogRoute.route("/").get(getBlogs).post(createBlog);
-
-export default blogRoute;
